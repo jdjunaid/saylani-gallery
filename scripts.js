2055edaba904bf929971d355264751bb990bf369
Ali <ali@hotmail.com>
2016-08-13 12:13:35 +0500

    gallery completed till onhover implementation

---
+++ scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('._1').on("mouseover",function(){
+		$('.main-image').css({"background-image":"url('bg.jpg')"});
+	});
+	$('._2').on("mouseover",function(){
+		$('.main-image').css({"background-image":"url('office.jpg')"});
+	});
+});
